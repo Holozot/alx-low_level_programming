8f56dabab1c97f9c77f7fc2e00520abb202ed79f
Holozot <kelwgra17@gmail.com>
2022-12-16 03:32:07 -0800

    Adapting betty style in file 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,7 +2,7 @@
 
 /**
  * more_numbers - prints 0 to 14 ten times
- * Return: 0 to 14 ten times 
+ * Return: 0 to 14 ten times
  */
 void more_numbers(void)
 {
@@ -10,7 +10,7 @@ void more_numbers(void)
 
 	for (k = 0; k < 10; k++)
 	{
-		for (l = 0; l <=14; l++)
+		for (l = 0; l <= 14; l++)
 		{
 			if (l >= 10)
 				_putchar(l / 10 + '0');
